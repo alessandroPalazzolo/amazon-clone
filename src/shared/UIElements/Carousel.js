bc64a27ebb422436f422cf0f001f13572ead65f8
alessandroPalazzolo <alessandrorochepalazzolo@gmail.com>
2021-01-10 23:44:21 +0100

    main-carousel automated and fixed

--- src/shared/UIElements/Carousel.js
+++ src/shared/UIElements/Carousel.js
@@ -1,23 +1,89 @@
-import React, { useState } from "react";
+import React, { useReducer, useEffect,  Children } from "react";
 
 import Button from "../FormElements/Button";
 
 import "./Carousel.scss";
 
+const carouselReducer = (state, action) => {
+   switch (action.type) {
+      case "ALL":
+         return({
+            ...state,
+            index: action.newIndex,
+            transition: action.newTransition
+         })
+      case "INDEX": 
+         return({
+            ...state,
+            index: action.newIndex
+         });
+      case "TRANSITION":
+         return({
+            ...state,
+            transition: action.newTransition
+         })
+      default:
+         return state;
+   }
+}
+
 const Carousel = props => {
 
-   const { items } = props;
-   const l = items.length;
-   const [ carouselState, setCarouselState ] = useState(Math.floor(Math.random() * l));
+   const l = Children.count(props.children);
 
-   const leftClickHandler = () => setCarouselState( !carouselState ? 5 : carouselState-1 );
+   const [ carouselState, dispatch ] = useReducer(carouselReducer, {
+      index: Math.floor(Math.random() * l),
+      transition: "transform 250ms ease-in-out"
+   });
+
+   const styles = {
+      transform: `translateX(${-carouselState.index * 100}%)`,
+      transition: `${carouselState.transition}`
+   }
+
+   useEffect(() => {
+      const automateCarousel = setTimeout(() => dispatch({
+         type: "ALL",
+         newIndex: carouselState.index + 1,
+         newTransition: "transform 250ms ease-in-out"
+      }), 6000);
+      return () => clearTimeout(automateCarousel);
+   }, [carouselState]);
+
+   const leftClickHandler = () => {
+      dispatch({
+         type: "ALL", 
+         newIndex: carouselState.index-1,
+         newTransition: "transform 250ms ease-in-out"
+      });
+   } 
    const rightClickHandler = () => {
-      setCarouselState( (carouselState+1)%l );
+      dispatch({
+         type: "ALL", 
+         newIndex: carouselState.index+1,
+         newTransition: "transform 250ms ease-in-out"
+      });
+   } 
+
+   const transitionHandler = () => {
+      if (carouselState.index == l-1 || carouselState.index == 0) {
+         dispatch({
+            type: "ALL",
+            newTransition: "none",
+            newIndex: carouselState.index ? 1 : l-2
+         });
+      }
    }
 
    return (
       <div className={`carousel ${props.className}`}>
-         <img alt="carouselItem" src={items[carouselState]}></img>
+         <div
+            className={`carousel__slider`}
+            style={styles}
+            onTransitionEnd={transitionHandler}
+         >
+            {props.children}
+         </div>
          <Button
             className="carousel__left-arrow"
             onClick={leftClickHandler}
